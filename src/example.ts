3bfcc966118f7c62e4ca5423c50924103cc966aa
Ali Almoullim <ali.almoullim@tryparcel.com>
2021-03-24 11:39:05 +0300

    *

---
+++ src/example.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+import path from "path";
+import { generate } from "./index";
+
+const html = generate({
+  title: "Registeration Completed",
+  message: "Hello Ali, welcome to Albaron and thank you for registering with us. Make your first order now, buon a petitti",
+  action: { name: "Show Menu", url: "http://google.com" },
+  tenant: "Albaron",
+  address: "98106-0167 O'Conner Burg 149 Quinn Gardens",
+  email: "Ebony.Rippin@gmail.com",
+  phone: "427-521-3577",
+  logo: "https://commarce-line.s3.me-south-1.amazonaws.com/media/60434c1dbaca28156517f9ae/website/logo",
+  image: "https://nectarsapp.s3.me-south-1.amazonaws.com/media/emails/registration.png",
+});
+
+fs.writeFileSync(path.join(__dirname, "test", "index.html"), html, { flag: "w" });
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import handlebars from "handlebars";
+import fs from "fs";
+import path from "path";
+
+export enum TEMPLATE {
+  BASE = "base.html",
+}
+
+type Data = {
+  title: string;
+  message: string;
+  action?: { name: string; url: string; colors?: { background: string; text: string } };
+  tenant: string;
+  address: string;
+  email: string;
+  phone: string;
+  logo: string;
+  image: string;
+};
+
+export const generate = (data: Data, template?: TEMPLATE): string => {
+  const file = fs.readFileSync(path.join(__dirname, "handlebars", template || TEMPLATE.BASE));
+  const compile = handlebars.compile(file.toString());
+
+  const html = compile(data);
+  return html;
+};
+
+export default generate;
